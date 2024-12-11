83aa0f38692c2dbe1f160a2b2bf243e08f174bad
Takeaio <iorizhunei2@gmail.com>
2024-12-11 14:37:08 +0900

    feat:ヘッダーの追加

---
+++ .history/frontend/components/Header/Header_20241211134521.tsx
@@ -0,0 +1,9 @@
+import React, { useState } from "react";
+
+export default function Header() {
+    return (
+      <header className="flex items-center justify-center p-4 border-b ">
+            <h1 className="text-xl font-semibold ">COCOMELO TALK</h1>
+      </header>
+    )
+  }
